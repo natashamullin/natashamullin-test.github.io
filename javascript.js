536ced2996a9a96c36e7b10e86c928d492c7df72
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-10-29 11:28:32 -0700

    updated test

--- javascript.js
+++ javascript.js
@@ -1,97 +1,102 @@
-const start = document.getElementById("timer");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-var counter = 0;
-var lastQuestionIndex = question.length -1;
-var runningQuestionIndex = 0;
-
-
-var questions = [
-    {
-        question: 'What is window.prompt?',
-        choiceA: 'a window that has text submitted by user with a button to submit',
-        choiceB: 'is a window with cancle/ok button',
-        choiceC: 'both A and B',
-        answer: 'A'
-    },
-    {
-        question: 'What is window.confirm?',
-        choiceA: 'a window that has text submitted by user with a button to submit.',
-        choiceB: 'is a window with cancle/ok button',
-        choiceC:'Both A nad B', 
-        answer: 'B',
-    },
-    { question: 'How do you know a function is a function?',
-    choiceA: '{}', 
-    choiceB: '()', 
-    choiceC: '[]',
-     answer: 'B' },
-    
-     { question: 'What signifies an array?',
-     choiceA: '{}',
-     choiceB: '()',
-     choiceC: '[] ', 
-     answer: 'C' },
-    
-    
-     { question: 'The DOM is built into the javascript language?',
-     choiceA: 'True', 
-     choiceB: 'False ',
-     choiceC: 'Neither true or false',
-     answer: 'B' },
-    
-     { question: 'What is a great tool used to see if our code is working like it should?',
-        choiceA: 'console.log',
-        choiceB: 'inspect',
-        choiceC: 'Google',
-         answer: 'a'
-    },
-]
-
-function renderQuestion(){
-var q= questions[runningQuestionIndex];
-question.innerHTML ="<p>" + q.question + "</p>";
-choiceA.innerHTML = q.choiceA;
-choiceB.innerHTML = q.choiceB;
-choiceC.innerHTML = q.choiceC;
-
-}
+var currentQuestion = {}
+var acceptAnswers = true
 var score = 0
-for (var i = 0; i < questions.length; i++) {
-    
-    if (question[runningQuestionIndex].correct ==questions[i].answer) {
-        score++;
-        //alert("Correct!");
-    } else { 
-        //alert("wrong :(");
+var questionCounter = 0
+var availableQuestions = []
+var startButton = document.getElementById('start-btn')
+var Questions = [
+
+
+    {
+        question: 'What is window.prompt?',
+        choice1: 'a window that has text submitted by user with a button to submit',
+        choice2: 'is a window with cancle/ok button',
+        choice3: 'both A and B',
+        answer: "is a window with cancle/ok button"
+    },
+
+
+    {
+        question: 'What is window.confirm?',
+        choice1: ' window that has text submitted by user with a button to submit',
+        choice2: 'is a window with cancle/ok button',
+        choice3: 'Both A nad B',
+        answer: 'is a window with cancle/ok button'
+    },
+
+    {
+        question: 'How do you know a function is a function?',
+        choice1: '{}',
+        choice2: '()',
+        choice3: '[]',
+        answer: '()'
+    },
+
+    {
+        question: 'What signifies an array?',
+
+        choice1: '{}',
+        choice2: '()',
+        choice3: '[]',
+        answer: '[]'
+    },
+
+    {
+        question: 'The DOM is built into the javascript language?',
+        choice1: 'True',
+        choice2: 'False ',
+        choice3: 'Neither true or false',
+        answer: 'false'
+    },
+
+    {
+        question: 'What is a great tool used to see if our code is working like it should?',
+        choice1: 'console.log',
+        choice2: 'inspect',
+        choice3: 'Google',
+        answer: 'console.log'
     }
-    //alert("you got" + score + "/" + questions.length);
+]
+var SCORE_POINTS = 1
+var MAX_QUESTIONS = 6
+startButton.addEventListener('click', startQuiz)
+
+startQuiz = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    startTimer()
+    getNewQuestion()
 
 }
 
 
-
-start.addEventListener("submit", questions);
-{
-    const timeLeftDisplay = document.querySelector('#time-left')
-    const startBtn = document.querySelector('#start')
-    var timeLeft = 300
-
-    function startTimer() {
-        setInterval(function () {
-            if (timeLeft <= 0) {
-                clearInterval(timeLeft = 0)
-                alert(" You ran out of time! better luck next time!")
-            }
-
-
-
-            timeLeftDisplay.innerHTML = timeLefttimeLeft -= 1
-        }, 1000)
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTION) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
     }
+
+    questionCounter++
+
+    var questionsIndex = math.floor(math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        var number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
     
 };
+
+
+
+
+
+
+
+
+
+
