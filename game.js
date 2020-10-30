0be6dcb7203b1d4be40b2b86f6ed707cac199b22
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-10-29 17:15:44 -0700

    the game is complete

--- game.js
+++ game.js
@@ -1,22 +1,22 @@
 const question = document.querySelector('#question');
-const choices = Array.form(document.querySelectorAll('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const question = document.querySelector('#question');
 
-let currentQuestion={};
+
+let currentQuestion = {};
 let acceptingAnswers = true;
-let score =0
-let questionCouner = 0
+let score = 0
+let questionCounter = 0
 let availableQuestions = []
 
-let question = [
-    { 
+let questions = [
+    {
         question: 'What is window.prompt?',
         choice1: 'a window that has text submitted by user with a button to submit',
         choice2: 'is a window with cancle/ok button',
         choice3: 'both A and B',
-        answer: 2 ,
+        answer: 2,
     },
 
 
@@ -25,7 +25,7 @@ let question = [
         choice1: ' window that has text submitted by user with a button to submit',
         choice2: 'is a window with cancle/ok button',
         choice3: 'Both A nad B',
-        answer: 2, 
+        answer: 2,
     },
 
     {
@@ -61,3 +61,62 @@ let question = [
         answer: 1,
     }
 ]
+
+const SCORE_POINTS = 1
+const MAX_QUESTIONS = 6
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter ++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    const questionIndex = Math.floor(Math.random() *availableQuestions.length)
+    currentQuestion =availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach (choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    
+    })
+
+availableQuestions.splice(questionIndex, 1)
+    acceptingAnswers = true
+}
+
+choices.forEach (choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+    
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    if(selectedAnswer === 'answer') {
+        incrementScore(SCORE_POINTS)
+    }
+setTimeout(() => {
+    getNewQuestion ()
+}, 1000)
+    
+})
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+
+}
+
+startGame()
